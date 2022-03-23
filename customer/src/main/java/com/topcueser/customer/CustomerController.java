0dd9204fd81fb8d742bdd6c3bdfe1c6a725d839c
topcueser <topcueser@gmail.com>
2022-03-24 00:50:48 +0300

    Spring Cloud Config Server

--- customer/src/main/java/com/topcueser/customer/CustomerController.java
+++ customer/src/main/java/com/topcueser/customer/CustomerController.java
@@ -1,6 +1,7 @@
 package com.topcueser.customer;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
 
     private final CustomerService customerService;
+    @Value("${custom.message}")
+    private String configServerCustomMessage;
 
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
@@ -20,6 +23,7 @@ public class CustomerController {
     @PostMapping
     public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest) {
         log.info("new customer registration {}", customerRegistrationRequest);
+        log.warn("Custom Message {}", configServerCustomMessage);
         customerService.registerCustomer(customerRegistrationRequest);
     }
 }
